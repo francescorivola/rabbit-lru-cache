5ffb49119cd10a715db8fe58153c5f03f5e238c8
Francesco Rivola <francesco.rivola@xepient.com>
2020-04-15 19:04:58 +0200

    export types from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-import { createRabbitLRUCache } from "./rabbit-lru-cache";
+import { 
+    createRabbitLRUCache, 
+    RabbitLRUCache as RabbitLRUCacheType, 
+    RabbitLRUCacheOptions as RabbitLRUCacheOptionsType 
+} from "./rabbit-lru-cache";
+
+export type RabbitLRUCache<T> = RabbitLRUCacheType<T>;
+export type RabbitLRUCacheOptions<T> = RabbitLRUCacheOptionsType<T>;
 export default createRabbitLRUCache;
